c2c26d4d4128800116ec573d1f5f8ed4f92e754f
Solomon <solomonknudson@gmail.com>
2026-05-22 23:37:19 -0400

    add more traits

--- include/type_traits/type_traits.hpp
+++ include/type_traits/type_traits.hpp
@@ -4,22 +4,10 @@
 
 namespace type_traits
 {
-  template <typename Invoker, typename... Args>
-  using EnableIfInvocable =
-      std::enable_if_t<std::is_invocable_v<Invoker, Args...>>;
-
-  template <typename T> using remove_reference = std::remove_reference_t<T>;
-
-  template <typename T>
-  using remove_cv_reference = std::remove_cv_t<remove_reference<T>>;
-
-  template <typename T> using remove_pointer = std::remove_pointer_t<T>;
 
   template <typename, typename = void> struct can_deref : std::false_type
   {
   };
-
-  // Custom traits
   template <typename T>
   struct can_deref<T, std::void_t<decltype(*std::declval<T>())>>
     : std::true_type
@@ -27,6 +15,26 @@ namespace type_traits
   };
 
   template <typename T> constexpr bool can_deref_v{can_deref<T>::value};
+
+  template <typename Function, typename... Args>
+  constexpr bool all_args_invocable_v{
+      (std::is_invocable_v<Function, Args> && ...)};
+
+  template <bool Condition> using enable_if = std::enable_if_t<Condition>;
+
+  template <typename Invoker, typename... Args>
+  using enable_if_invocable = enable_if<std::is_invocable_v<Invoker, Args...>>;
+
+  template <typename Invoker, typename... Args>
+  using enable_if_all_args_invocable =
+      enable_if<all_args_invocable_v<Invoker, Args...>>;
+
+  template <typename T> using remove_reference = std::remove_reference_t<T>;
+
+  template <typename T>
+  using remove_cv_reference = std::remove_cv_t<remove_reference<T>>;
+
+  template <typename T> using remove_pointer = std::remove_pointer_t<T>;
 } // namespace type_traits
 #endif // TYPE_TRAITS_WRAPPER_HPP
 
