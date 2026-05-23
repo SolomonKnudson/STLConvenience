d67f54441b484ca6bb2533bb4bf019163339f1e7
Solomon <solomonknudson@gmail.com>
2026-05-22 23:45:21 -0400

    rename traits

--- include/type_traits/type_traits.hpp
+++ include/type_traits/type_traits.hpp
@@ -17,17 +17,18 @@ namespace type_traits
   template <typename T> constexpr bool can_deref_v{can_deref<T>::value};
 
   template <typename Function, typename... Args>
-  constexpr bool all_args_invocable_v{
+  constexpr bool separate_args_invocable_v{
       (std::is_invocable_v<Function, Args> && ...)};
 
   template <bool Condition> using enable_if = std::enable_if_t<Condition>;
 
   template <typename Invoker, typename... Args>
-  using enable_if_invocable = enable_if<std::is_invocable_v<Invoker, Args...>>;
+  using enable_if_arg_pack_invocable =
+      enable_if<std::is_invocable_v<Invoker, Args...>>;
 
   template <typename Invoker, typename... Args>
-  using enable_if_all_args_invocable =
-      enable_if<all_args_invocable_v<Invoker, Args...>>;
+  using enable_if_separate_args_invocable =
+      enable_if<separate_args_invocable_v<Invoker, Args...>>;
 
   template <typename T> using remove_reference = std::remove_reference_t<T>;
 
