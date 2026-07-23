db41c9e30a4e7221a73b2cdc9a103ead996eb774
Solomon <solomonknudson@gmail.com>
2026-07-22 23:20:19 -0400

    no double durations.....

--- include/STLConvenience/internal/chrono/make_duration.hpp
+++ include/STLConvenience/internal/chrono/make_duration.hpp
@@ -4,18 +4,9 @@
 
 namespace STLC::chrono
 {
-  using hours_d = std::chrono::duration<double, std::ratio<3600>>;
-  using minutes_d = std::chrono::duration<double, std::ratio<60>>;
-
-  using seconds_d = std::chrono::duration<double, std::ratio<1>>;
-  using milliseconds_d = std::chrono::duration<double, std::milli>;
-
-  using microseconds_d = std::chrono::duration<double, std::micro>;
-  using nanoseconds_d = std::chrono::duration<double, std::nano>;
-
-  template <typename Duration, typename Rep>
+  template <typename Duration>
   constexpr Duration
-  make_duration(const Rep count)
+  make_duration(const typename Duration::rep count)
   {
     return Duration{count};
   }
@@ -54,42 +45,6 @@ namespace STLC::chrono
   make_nanoseconds(const std::chrono::nanoseconds::rep count)
   {
     return make_duration<std::chrono::nanoseconds>(count);
-  }
-
-  static constexpr hours_d
-  make_hours(const double count)
-  {
-    return make_duration<hours_d>(count);
-  }
-
-  static constexpr minutes_d
-  make_minutes(const double count)
-  {
-    return make_duration<minutes_d>(count);
-  }
-
-  static constexpr seconds_d
-  make_seconds(const double count)
-  {
-    return make_duration<seconds_d>(count);
-  }
-
-  static constexpr milliseconds_d
-  make_milliseconds(const double count)
-  {
-    return make_duration<milliseconds_d>(count);
-  }
-
-  static constexpr microseconds_d
-  make_microseconds(const double count)
-  {
-    return make_duration<microseconds_d>(count);
-  }
-
-  static constexpr nanoseconds_d
-  make_nanoseconds(const double count)
-  {
-    return make_duration<nanoseconds_d>(count);
   }
 } // namespace STLC::chrono
 #endif // STLC_INTERNAL_CHRONO_MAKE_DURATION_HPP
