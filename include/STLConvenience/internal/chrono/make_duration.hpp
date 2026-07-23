f3e622b5c7055afc00b5ab71f50aa6bfc60a9dd7
Solomon <solomonknudson@gmail.com>
2026-07-22 23:13:31 -0400

    last?

--- include/STLConvenience/internal/chrono/make_duration.hpp
+++ include/STLConvenience/internal/chrono/make_duration.hpp
@@ -1,59 +1,95 @@
 #ifndef STLC_INTERNAL_CHRONO_MAKE_DURATION_HPP
 #define STLC_INTERNAL_CHRONO_MAKE_DURATION_HPP
-#include <STLConvenience/type_traits.hpp>
 #include <chrono>
 
 namespace STLC::chrono
 {
-  template <typename ReturnDuration,
-            typename Rep,
-            typename = STLC::type_traits::enable_if<std::is_arithmetic_v<Rep>>>
-  constexpr ReturnDuration
+  using hours_d = std::chrono::duration<double, std::ratio<3600>>;
+  using minutes_d = std::chrono::duration<double, std::ratio<60>>;
+
+  using seconds_d = std::chrono::duration<double, std::ratio<1>>;
+  using milliseconds_d = std::chrono::duration<double, std::milli>;
+
+  using microseconds_d = std::chrono::duration<double, std::micro>;
+  using nanoseconds_d = std::chrono::duration<double, std::nano>;
+
+  template <typename Duration, typename Rep>
+  constexpr Duration
   make_duration(const Rep count)
   {
-    return ReturnDuration{count};
+    return Duration{count};
   }
 
-  template <typename Rep = std::chrono::hours::rep>
   static constexpr std::chrono::hours
-  make_hours(const Rep count)
+  make_hours(const std::chrono::hours::rep count)
   {
     return make_duration<std::chrono::hours>(count);
   }
 
-  template <typename Rep = std::chrono::minutes::rep>
   static constexpr std::chrono::minutes
-  make_minutes(const Rep count)
+  make_minutes(const std::chrono::minutes::rep count)
   {
     return make_duration<std::chrono::minutes>(count);
   }
 
-  template <typename Rep = std::chrono::seconds::rep>
   static constexpr std::chrono::seconds
-  make_seconds(const Rep count)
+  make_seconds(const std::chrono::seconds::rep count)
   {
     return make_duration<std::chrono::seconds>(count);
   }
 
-  template <typename Rep = std::chrono::milliseconds::rep>
   static constexpr std::chrono::milliseconds
-  make_milliseconds(const Rep count)
+  make_milliseconds(const std::chrono::milliseconds::rep count)
   {
     return make_duration<std::chrono::milliseconds>(count);
   }
 
-  template <typename Rep = std::chrono::milliseconds::rep>
   static constexpr std::chrono::microseconds
-  make_microseconds(const Rep count)
+  make_microseconds(const std::chrono::microseconds::rep count)
   {
     return make_duration<std::chrono::microseconds>(count);
   }
 
-  template <typename Rep = std::chrono::nanoseconds::rep>
   static constexpr std::chrono::nanoseconds
-  make_nanoseconds(const Rep count)
+  make_nanoseconds(const std::chrono::nanoseconds::rep count)
   {
     return make_duration<std::chrono::nanoseconds>(count);
+  }
+
+  static constexpr hours_d
+  make_hours(const double count)
+  {
+    return make_duration<hours_d>(count);
+  }
+
+  static constexpr minutes_d
+  make_minutes(const double count)
+  {
+    return make_duration<minutes_d>(count);
+  }
+
+  static constexpr seconds_d
+  make_seconds(const double count)
+  {
+    return make_duration<seconds_d>(count);
+  }
+
+  static constexpr milliseconds_d
+  make_milliseconds(const double count)
+  {
+    return make_duration<milliseconds_d>(count);
+  }
+
+  static constexpr microseconds_d
+  make_microseconds(const double count)
+  {
+    return make_duration<microseconds_d>(count);
+  }
+
+  static constexpr nanoseconds_d
+  make_nanoseconds(const double count)
+  {
+    return make_duration<nanoseconds_d>(count);
   }
 } // namespace STLC::chrono
 #endif // STLC_INTERNAL_CHRONO_MAKE_DURATION_HPP
